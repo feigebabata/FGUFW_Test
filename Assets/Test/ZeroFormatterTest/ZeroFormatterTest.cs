e234466dc1dd35210b6c2fedb51eb08e17aa02c0
feigebabata <feigebabata@outlook.com>
2022-10-21 10:43:04 +0800

    添加定点数物理库

--- Assets/Test/ZeroFormatterTest/ZeroFormatterTest.cs
+++ Assets/Test/ZeroFormatterTest/ZeroFormatterTest.cs
@@ -8,12 +8,13 @@ public class ZeroFormatterTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        byte[] buffer = new byte[512];
         var data = new Data(123,1);
-        var buffer = ZeroFormatterSerializer.Serialize(data);
+        var length = ZeroFormatterSerializer.Serialize(ref buffer,0,data);
 
-        data = ZeroFormatterSerializer.Deserialize<Data>(buffer);
+        data = ZeroFormatterSerializer.Deserialize<Data>(buffer,0);
 
-        Debug.Log($"id={data.Id} len={buffer.Length}");
+        Debug.Log($"id={data.Id} len={length}");
     }
 
     // Update is called once per frame
