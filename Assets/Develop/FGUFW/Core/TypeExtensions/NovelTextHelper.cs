161901eaa81ae11f69517dd0d3fe25dcef95811d
feigebabata <feigebabata@outlook.com>
2022-12-17 12:41:30 +0800

    1.修改小说工具

--- Assets/Develop/FGUFW/Core/TypeExtensions/NovelTextHelper.cs
+++ Assets/Develop/FGUFW/Core/TypeExtensions/NovelTextHelper.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using UnityEngine;
 
 namespace FGUFW
 {
     public static class NovelTextHelper
     {
+        public readonly static Encoding GB2312 = Encoding.GetEncoding("gb2312");
         private const string CHAPER_END = "章卷篇节回话部集季";
         private const int CHAPER_MAX_LENGTH = 25;
         private const string LINE_END = ";；。!！」…”\"》）\\-※";
@@ -17,10 +20,57 @@ namespace FGUFW
         private static Regex regexChapter3_2 = new Regex($"（[{NUMBER}]+-[{NUMBER}]+）$");
 
 
-        public static string[][] SplitChapter(string[] lines)
+        public static Vector2Int[] SplitChapter(string[] lines)
         {
-            
-            return null;
+            List<Vector2Int> indexs = new List<Vector2Int>(lines.Length/100);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if(LineIsChapter(lines[i]))
+                {
+                    var last = indexs[indexs.Count-1];
+                    last.y = i-last.x;
+                    indexs[indexs.Count-1] = last;
+
+                    indexs.Add(new Vector2Int(i,0));
+                }
+                else if(i==0)
+                {
+                    indexs.Add(new Vector2Int(i,0));
+                }
+                else if(i==lines.Length-1)
+                {
+                    var last = indexs[indexs.Count-1];
+                    last.y = i-last.x;
+                    indexs[indexs.Count-1] = last;
+                    
+                }
+            }
+            return indexs.ToArray();
+        }
+
+        public static string[] FormatLines(string[] lines)
+        {
+            List<string> ls = new List<string>(lines.Length);
+            foreach (var line in lines)
+            {
+                if(string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line))continue;
+                
+                if(LineIsChapter(line))
+                {
+                    ls.Add($"{line.Trim()}");
+                }
+                else
+                {
+                    ls.Add($"    {line.Trim()}");
+                }
+                ls.Add(string.Empty);
+            }
+            return ls.ToArray();
+        }
+
+        public static string GetChapter(string lines,Vector2Int chapterIndex)
+        {
+            return string.Join("\n",lines,chapterIndex.x,chapterIndex.y);
         }
 
         public static bool LineIsChapter(string line)
@@ -36,6 +86,13 @@ namespace FGUFW
         public static bool LineIsEnd(string line)
         {
             return regexLineEnd.IsMatch(line);
+        }
+
+        public static int GetTextLength(Font font,int size,Vector2 rect,string text,int index)
+        {
+            int length = 0;
+            
+            return length;
         }
 
 
--- Assets/Test/NovelTextTest/NovelTextTest.cs
+++ Assets/Test/NovelTextTest/NovelTextTest.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FGUFW;
+using UnityEngine.UI;
 
 public class NovelTextTest : MonoBehaviour
 {
-    public string Line;
+    public Text textComp;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(NovelTextHelper.LineIsChapter(Line));
+        textComp.font.RequestCharactersInTexture(textComp.text,textComp.fontSize);
+        foreach (var item in textComp.text)
+        {
+            CharacterInfo info;
+            if(textComp.font.GetCharacterInfo(item,out info,textComp.fontSize))
+            {
+                Debug.Log(info.width);
+            }
+        }
     }
     
 }
