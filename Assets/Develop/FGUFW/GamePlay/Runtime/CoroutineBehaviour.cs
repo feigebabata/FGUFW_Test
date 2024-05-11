dd4584b4ddeed27f80a6171ac6a5bd6126d3cdab
feigebabata <feigebabata@outlook.com>
2024-05-11 18:02:41 +0800

    1.整理mono分支 2.规划架构设计

--- Assets/Develop/FGUFW/GamePlay/Runtime/CoroutineBehaviour.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace FGUFW.GamePlay
-{
-    [DisallowMultipleComponent]
-    public class CoroutineBehaviour:MonoBehaviour{};
-}
--- Assets/Develop/FGUFW/Net/Runtime/PortConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace FGUFW.Net
-{
-    public static class PortConfig
-    {
-        public const int BROADCAST_RECEIVE = 31410;
-    }
-}
