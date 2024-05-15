d6774df367b6d1f93a912257616de22bfe1c482c
feigebabata <feigebabata@outlook.com>
2024-05-15 18:32:07 +0800

    更新框架设计

---
+++ Assets/FGUFW/Core/Helper/CharHelper.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace FGUFW
+{
+    /// <summary>
+    /// 磁盘路径助手
+    /// </summary>
+    public sealed class CharHelper
+    {
+        public const char U265E = '♞';
+        public const char U263B = '☻';
+        public const char U2251 = '☹';
+        public const char U27B6 = '➶';
+        public const char U27BB = '➻';
+        public const char U2710 = '✐';
+    }
+}
