398f17dfcc868ee9dfae1e9857ebb0f8a3bfa47a
feigebabata <feigebabata@outlook.com>
2025-06-24 17:36:37 +0800

    扩展ui对象创建工具 扩展excel转换工具 修改monogameplay界面预制件创建

--- Assets/FGUFW/ExceUtils/Editor/ExcelCsharpToJson.cs
+++ Assets/FGUFW/ExceUtils/Editor/ExcelCsharpToJson.cs
@@ -14,96 +14,96 @@ namespace FGUFW.ExcelUtils
 {
     public static class ExcelCsharpToJson
     {
-        public static void ToJson(Excel excel,string path)
+        public static void ToJson(Excel excel, string path)
         {
             var name = Path.GetFileNameWithoutExtension(path);
             var jsonBuilder = new StringBuilder();
 
-            jsonBuilder.Append('{'); 
+            jsonBuilder.Append('{');
             for (int i = 0; i < excel.SheetCount; i++)
             {
                 var sheet = excel[i];
-                
+
                 try
                 {
-                    sheetToJson(sheet,jsonBuilder);
+                    sheetToJson(sheet, jsonBuilder);
                 }
                 catch (System.Exception ex)
                 {
                     Debug.LogError($"ExcelCsharpToJson:{name}.{sheet.SheetName} \n{ex.Message}\n{ex.StackTrace}");
                 }
-                
-                if(i<excel.SheetCount-1)
+
+                if (i < excel.SheetCount - 1)
                 {
                     jsonBuilder.Append(',');
                 }
             }
             jsonBuilder.Append('}');
 
-            var directory = Path.Combine(Application.dataPath,"ECJsonData");
+            var directory = Path.Combine(Application.dataPath, "ECJsonData");
 
 
-            if(!Directory.Exists(directory))
+            if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
             }
-            
-            File.WriteAllText(Path.Combine(directory,$"{name}.json"),jsonBuilder.ToString());
+
+            File.WriteAllText(Path.Combine(directory, $"{name}.json"), jsonBuilder.ToString());
         }
 
         private static void sheetToJson(ISheet sheet, StringBuilder jsonBuilder)
         {
             var firstRow = sheet.GetRow(0);
-            if(firstRow==default)return;
+            if (firstRow == default) return;
 
             //第一行第一列 校验标识
-            if(firstRow.GetCell(0)?.ToString() != "GenerateExcelCsharpCode")return;
+            if (firstRow.GetCell(0)?.ToString() != "GenerateExcelCsharpCode") return;
 
             var collection = firstRow.GetCell(1)?.ToString();
-            if(collection.IsNull())return;
+            if (collection.IsNull()) return;
 
-            jsonBuilder.Append($"\"{sheet.SheetName}s\":");
 
             //第三行字段类型
             var types = sheet.GetRow(2);
             //第四行字段名
             var names = sheet.GetRow(3);
 
-            int maxRowIdx = sheet.LastRowNum+1;
+            int maxRowIdx = sheet.LastRowNum + 1;
             int maxCellIdx = sheet.GetRow(1).LastCellNum;
-            
+
             //过滤有效列 type列为空则忽略
             var cellIdxs = new List<int>(maxCellIdx);
             for (int ci = 0; ci < maxCellIdx; ci++)
             {
                 var tVar = types.GetCell(ci);
-                if(tVar==default)continue;
+                if (tVar == default) continue;
                 cellIdxs.Add(ci);
             }
 
-            if(collection == "List")
+            if (collection == "List")
             {
+                jsonBuilder.Append($"\"{sheet.SheetName}s\":");
                 jsonBuilder.Append('[');
 
                 for (int ri = 4; ri < maxRowIdx; ri++)
                 {
                     var row = sheet.GetRow(ri);
-                    
+
                     jsonBuilder.Append('{');
 
                     for (int i = 0; i < cellIdxs.Count; i++)
                     {
                         var ci = cellIdxs[i];
                         jsonBuilder.Append($"\"{names.GetCell(ci)}\":");
-                        jsonBuilder.Append(getValueByType(types.GetCell(ci).ToString(),row.GetCell(ci)?.ToString()));
-                        if(i<cellIdxs.Count-1)
+                        jsonBuilder.Append(getValueByType(types.GetCell(ci).ToString(), row.GetCell(ci)?.ToString()));
+                        if (i < cellIdxs.Count - 1)
                         {
                             jsonBuilder.Append(',');
                         }
                     }
                     jsonBuilder.Append('}');
 
-                    if(ri<maxRowIdx-1)
+                    if (ri < maxRowIdx - 1)
                     {
                         jsonBuilder.Append(',');
                     }
@@ -111,8 +111,9 @@ namespace FGUFW.ExcelUtils
 
                 jsonBuilder.Append(']');
             }
-            else if(collection == "Table")
+            else if (collection == "Table")
             {
+                jsonBuilder.Append($"\"{sheet.SheetName}s\":");
                 jsonBuilder.Append('{');
 
                 for (int ri = 4; ri < maxRowIdx; ri++)
@@ -120,77 +121,105 @@ namespace FGUFW.ExcelUtils
                     var row = sheet.GetRow(ri);
 
                     jsonBuilder.Append($"\"{row.GetCell(0)}\":");
-                    
+
                     jsonBuilder.Append('{');
                     for (int i = 0; i < cellIdxs.Count; i++)
                     {
                         var ci = cellIdxs[i];
 
                         jsonBuilder.Append($"\"{names.GetCell(ci)}\":");
-                        jsonBuilder.Append(getValueByType(types.GetCell(ci).ToString(),row.GetCell(ci)?.ToString()));
-                        if(i<cellIdxs.Count-1)
+                        jsonBuilder.Append(getValueByType(types.GetCell(ci).ToString(), row.GetCell(ci)?.ToString()));
+                        if (i < cellIdxs.Count - 1)
                         {
                             jsonBuilder.Append(',');
                         }
                     }
                     jsonBuilder.Append('}');
 
-                    if(ri<maxRowIdx-1)
+                    if (ri < maxRowIdx - 1)
                     {
                         jsonBuilder.Append(',');
                     }
                 }
 
                 jsonBuilder.Append('}');
-            }      
+            }
+            else if (collection == "Single")
+            {
+                jsonBuilder.Append($"\"{sheet.SheetName}Single\":");
+                jsonBuilder.Append('{');
+
+                for (int ri = 2; ri < maxRowIdx; ri++)
+                {
+                    var row = sheet.GetRow(ri);
+
+                    jsonBuilder.Append($"\"{row.GetCell(1)}\":");
+                    jsonBuilder.Append(getValueByType(row.GetCell(0).ToString(), row.GetCell(2)?.ToString()));
+
+                    if (ri < maxRowIdx - 1)
+                    {
+                        jsonBuilder.Append(',');
+                    }
+                }
+
+                jsonBuilder.Append('}');
+            }
 
         }
 
-        static string getValueByType(string type,string value)
+        static string getValueByType(string type, string value)
         {
             switch (type)
             {
                 case "int":
-                {
-                    if(value.IsNull())
                     {
-                        return "0";
+                        if (value.IsNull())
+                        {
+                            return "0";
+                        }
+                        else
+                        {
+                            return value;
+                        }
                     }
-                    else
-                    {
-                        return value;
-                    }
-                }
                 case "float":
-                {
-                    if(value.IsNull())
                     {
-                        return "0.0";
+                        if (value.IsNull())
+                        {
+                            return "0.0";
+                        }
+                        else
+                        {
+                            return value;
+                        }
                     }
-                    else
-                    {
-                        return value;
-                    }
-                }
                 case "bool":
-                {
-                    if(value.IsNull())
                     {
-                        return "false";
+                        if (value.IsNull())
+                        {
+                            return "false";
+                        }
+                        else
+                        {
+                            if (value == "0")
+                            {
+                                return "false";
+                            }
+                            else if (value == "1")
+                            {
+                                return "true";
+                            }
+                            return value;
+                        }
                     }
-                    else
-                    {
-                        return value;
-                    }
-                }
                 case "string":
-                {
-                    return $"\"{value}\"";
-                }
-                
+                    {
+                        return $"\"{value}\"";
+                    }
+
                 default:
                     Debug.LogError($"未知类型:[{type}]");
-                break;
+                    break;
             }
 
             return default;
